385fece566a02d35a432f10c36c54715f4b00ab9
Rima O <rymaothmani@gmail.com>
2020-01-10 23:59:12 -0600

    create questions.js and load all the questions and answers for the quizz.

---
+++ questions.js
@@ -0,0 +1,26 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title:"Inside which HTML element do we put the JavaScript?",
+        choices: ["<script>", "<javascript>", "<js>", "<element>" ],
+        answer: "<script>"
+    },
+    {
+        title:"How do you write \"Hello World\" in an alert box?",
+        choices: ["msg(\"Hello World\")", "msgBox(\"Hello World\")", "alert(\"Hello World\")"],
+        answer: "alert(\"Hello World\")"
+    },
+    {
+        title:"Where is Waldo?",
+        choices: ["Antartica", "studying to be a web developer", "minding his own business"],
+        answer: "studying to be a web developer"
+    };
